94774aa6e419f03c8dc2c326eb3506a9fcb30fbf
kentkim84 <kentkim84@gmail.com>
2017-09-27 22:04:42 +0100

    done displaying the current time

---
+++ 02-CurrentTime.go
@@ -0,0 +1,16 @@
+// Module: Data Representation
+// Tutor: Ian McLoughlin
+// Author: Yongjin Kim
+// Title: Current Time
+// Objective: Create a programme that prints the current time
+
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func main() {
+  fmt.Println("Current time is", time.Now())
+}
