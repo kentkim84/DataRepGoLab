3644ca83047bb9bcca5e26a33a0785282d1e72f4
kentkim84 <kentkim84@gmail.com>
2017-09-27 11:27:00 +0100

    done displaying hello world in japanese

--- 01-HelloWorld.go
+++ 01-HelloWorld.go
@@ -1,9 +1,14 @@
-// https://gobyexample.com/hello-world
+// Module: Data Representation
+// Tutor: Ian McLoughlin
+// Author: Yongjin Kim
+// Title: Hello, World! in Japanese
+// Objective: Create a programme that prints "Hello, World!" in Japanese
 
 package main
 
 import "fmt"
 
 func main() {
-  fmt.Println("Hello, world!")
+  fmt.Printf("English: Hello, World!\n")
+  fmt.Printf("Japanese: こんにちは, 世界!\n")
 }
